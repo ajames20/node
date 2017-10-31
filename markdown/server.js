61b0eb5f99ec2299d58594f72a2d07f0fb5b9f26
Andrew James <ajames20@gmail.com>
2017-10-31 11:49:11 +0200

    remove comments

--- markdown/server.js
+++ markdown/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// public folder to store assets
 app.use(express.static(__dirname + '/public'));
 
-// routes for app
 app.get('/', (req, res) => {
   res.render('pad');
 });
 
-// listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
 app.listen(port);
