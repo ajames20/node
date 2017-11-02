5c0c57b1c9f50e9ef181f4bac19eca1beb535d7a
Andrew James <ajames20@gmail.com>
2017-11-02 13:09:24 +0200

    add airbng style eselint config

--- markdown/server.js
+++ markdown/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(`${__dirname}/public`)));
 
 app.get('/', (req, res) => {
   res.render('pad');
 });
 
-var port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port);
