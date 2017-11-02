1a90f30e8754b80b00917f35d9f9427ddbb8daae
Andrew James <ajames20@gmail.com>
2017-11-02 12:23:29 +0200

    add twitter bot

---
+++ twitbot/src/bot.js
@@ -0,0 +1,18 @@
+const Twit = require('twit');
+const config = require('./config');
+
+const bot = new Twit(config);
+
+bot.post(
+  'statuses/update',
+  {
+    status: 'hello world! @JeromeHardaway you should follow me.'
+  },
+  (err, data, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`${data.text} tweeted!`);
+    }
+  }
+);
